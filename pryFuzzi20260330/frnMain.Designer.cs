5db6246a56494fbe71e4699f47bbbcbb4e03937c
Alumno <Alumno@PC107>
2026-03-30 08:48:33 -0300

    Agregar archivos de proyecto.

---
+++ pryFuzzi20260330/frnMain.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace pryFuzzi20260330
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDaily = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.tbxDaily = new System.Windows.Forms.TextBox();
+            this.tbxDate = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblDaily
+            // 
+            this.lblDaily.AutoSize = true;
+            this.lblDaily.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDaily.Location = new System.Drawing.Point(203, 171);
+            this.lblDaily.Name = "lblDaily";
+            this.lblDaily.Size = new System.Drawing.Size(97, 20);
+            this.lblDaily.TabIndex = 0;
+            this.lblDaily.Text = "Café del día:";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(203, 220);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(58, 20);
+            this.lblDate.TabIndex = 1;
+            this.lblDate.Text = "Fecha:";
+            // 
+            // tbxDaily
+            // 
+            this.tbxDaily.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxDaily.Location = new System.Drawing.Point(306, 168);
+            this.tbxDaily.Name = "tbxDaily";
+            this.tbxDaily.Size = new System.Drawing.Size(146, 26);
+            this.tbxDaily.TabIndex = 2;
+            // 
+            // tbxDate
+            // 
+            this.tbxDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxDate.Location = new System.Drawing.Point(306, 220);
+            this.tbxDate.Name = "tbxDate";
+            this.tbxDate.Size = new System.Drawing.Size(146, 26);
+            this.tbxDate.TabIndex = 3;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::pryFuzzi20260330.Properties.Resources.paredes_de_cafeteria_selva;
+            this.ClientSize = new System.Drawing.Size(660, 413);
+            this.Controls.Add(this.tbxDate);
+            this.Controls.Add(this.tbxDaily);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblDaily);
+            this.Name = "frmMain";
+            this.Text = "Cafetería ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDaily;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.TextBox tbxDaily;
+        private System.Windows.Forms.TextBox tbxDate;
+    }
+}
+
